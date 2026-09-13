b02323fca3938802d7e56dab604bebd0226cd2b8
joao-vitor-queiroz <joaov9.dev@gmail.com>
2026-09-13 17:01:15 +0000

    adicionando função de finalizar jogo e jogar novamente

--- jogo-dados/components/JogoDados.js
+++ jogo-dados/components/JogoDados.js
@@ -11,6 +11,7 @@ export default function JogoDados() {
   const [vitoriasJ1, setVitoriasJ1] = useState(0)
   const [vitoriasJ2, setVitoriasJ2] = useState(0)
   const [empates, setEmpates] = useState(0)
+  const [jogoFinalizado, setJogoFinalizado] = useState(false)
 
   function valor() {
     return Math.floor(Math.random() * 6) + 1
@@ -22,6 +23,7 @@ export default function JogoDados() {
 
     setDadosJ1([d1, d2])
     setTurno(2)
+    setMensagem("")
    }
 
   function jogarJ2() {
@@ -38,17 +40,43 @@ export default function JogoDados() {
     const somaJ2 = d1 + d2
 
     if (somaJ1 > somaJ2) {
-      setMensagem("Vitoria do Jogador 1")
+      setMensagem("Jogador 1 venceu a rodada")
       setVitoriasJ1(vitoriasJ1 + 1)
     } else if (somaJ2 > somaJ1) {
-      setMensagem("Vitoria do Jogador 2")
+      setMensagem("Jogador 2 venceu a rodada")
       setVitoriasJ2(vitoriasJ2 + 1)
     } else {
-      setMensagem("Empate")
+      setMensagem("Rodada empatada")
       setEmpates(empates + 1)
     }
 
-    setRodada(rodada + 1)
+    if (rodada == 5) {
+      setJogoFinalizado(true)
+    } else {
+      setRodada(rodada + 1)
+    }
+  }
+
+  function finalizarJogo() {
+    if (vitoriasJ1 > vitoriasJ2) {
+      return "Jogador 1 venceu o jogo"
+    } else if (vitoriasJ2 > vitoriasJ1) {
+      return "Jogador 2 venceu o jogo"
+    } else {
+      return "Empate geral"
+    }
+  }
+
+  function jogarNovamente() {
+    setRodada(1)
+    setDadosJ1([1, 1])
+    setDadosJ2([1, 1])
+    setTurno(1)
+    setMensagem("")
+    setVitoriasJ1(0)
+    setVitoriasJ2(0)
+    setEmpates(0)
+    setJogoFinalizado(false)
   }
 
   return (
@@ -61,16 +89,23 @@ export default function JogoDados() {
         <h3>Jogador 1</h3>
         <Dado valor={dadosJ1[0]} />
         <Dado valor={dadosJ1[1]} />
-        <button onClick={jogarJ1} disabled={turno == 2}>Jogar</button>
+        <button onClick={jogarJ1} disabled={turno == 2 || jogoFinalizado}>Jogar</button>
       </div>
 
       <div className="jogador">
         <h3>Jogador 2</h3>
         <Dado valor={dadosJ2[0]} />
         <Dado valor={dadosJ2[1]} />
-        <button onClick={jogarJ2} disabled={turno == 1}>Jogar</button>
+        <button onClick={jogarJ2} disabled={turno == 1 || jogoFinalizado}>Jogar</button>
       </div>
       <p>Vitorias do Jogador 1: {vitoriasJ1} | Vitorias do Jogador 2: {vitoriasJ2} | Empates: {empates}</p>
+
+      {jogoFinalizado && (
+        <div className="resultadoFinal">
+          <h1>{finalizarJogo()}</h1>
+          <button onClick={jogarNovamente}>Jogar Novamente</button>
+        </div>
+      )}
     </div>
   )
 }
